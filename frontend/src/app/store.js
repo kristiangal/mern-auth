a3e6a0d52d3c2089912eaaf43cef28da7636259d
Kristián Gál <gal@essensworld.com>
2023-09-28 16:25:21 +0200

    add redux and init store and authSlice

---
+++ frontend/src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+
+const store = configureStore({
+  reducer: { auth: authReducer },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: true,
+});
+
+export default store;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,8 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+import store from "./app/store.js";
+import { Provider } from "react-redux";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./pages/HomePage.jsx";
@@ -24,7 +26,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
