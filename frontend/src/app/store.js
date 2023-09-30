e447bcc08765339ae01cfd553c15b45f33d6043e
Kristián Gál <gal@essensworld.com>
2023-09-30 13:01:32 +0200

    implement redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import { apiSlice } from "../features/apiSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: {
+    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
